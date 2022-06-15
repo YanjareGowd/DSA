8c69ffeef578ec6a78c725da1043a30dd0222ae0
YanjareGowd <106945034+YanjareGowd@users.noreply.github.com>
2022-06-15 11:52:56 +0530

    Create TreeRecursion.c

---
+++ recursion/TreeRecursion.c
@@ -0,0 +1,21 @@
+
+#include <stdio.h>
+#include <stdlib.h>
+
+int fun(int n)
+{
+    if(n>0)
+    {
+
+        printf("%d ",n);
+        fun(n-1);
+        fun(n-1);
+;    }
+return 0;
+}
+int main()
+{
+    int a=3;
+     fun(a);
+    return 0;
+}
